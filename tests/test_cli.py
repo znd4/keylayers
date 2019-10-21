a14518ff9609bae1ad12d1f81fac67e327062d9c
Zane DuFour <zanedufour@pop-os.localdomain>
2019-10-20 22:36:40 -0400

    test_service.py -> test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,7 +7,6 @@ import pytest
 
 from click.testing import CliRunner
 
-from keylayers import keylayers
 from keylayers import cli
 
 
@@ -30,9 +29,9 @@ def test_content(response):
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
+    result = runner.invoke(cli.cli)
     assert result.exit_code == 0
-    assert "keylayers.cli.main" in result.output
-    help_result = runner.invoke(cli.main, ["--help"])
+    assert "keylayers.cli.cli" in result.output
+    help_result = runner.invoke(cli.cli, ["--help"])
     assert help_result.exit_code == 0
     assert "--help  Show this message and exit." in help_result.output
