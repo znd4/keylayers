72450241933e07a1c8677fba59cc358cb5c1bef1
Zane DuFour <zanedufour@pop-os.localdomain>
2019-10-20 21:04:56 -0400

    Add fcntl import

--- keylayers/cli.py
+++ keylayers/cli.py
@@ -3,14 +3,17 @@
 """Console script for keylayers."""
 import sys
 import click
+import fcntl
+from .keylayers import run_state_machine
 
 
-@click.command()
+@click.group()
 def main(args=None):
     """Console script for keylayers."""
     click.echo("Replace this message by putting your code into "
                "keylayers.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
+    run_state_machine
     return 0
 
 
